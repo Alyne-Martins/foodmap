bb8cab50a2c32e049a143b41780306c066097fa0
Alyne Martins <martins.alyne@gmail.com>
2018-08-03 04:12:46 -0300

    inicialização pronta

--- js/app.js
+++ js/app.js
@@ -1,1 +1,67 @@
-// Add your JavaScript
+$(document).ready(function () {
+	$('.home').delay('5000').fadeIn('slow');
+	var boxLogo = $("#box-logo");
+	boxLogo.animate({
+		height: '250px'
+	}, "slow");
+	boxLogo.animate({
+		width: '250px',
+		opacity: '0.4'
+	}, "slow");
+	boxLogo.animate({
+		height: '50px',
+		width: '50px',
+		opacity: '0.6'
+	}, "slow");
+	boxLogo.animate({
+		width: '250px',
+		opacity: '1.0'
+	}, "slow");
+	boxLogo.animate({
+		width: '250px',
+		height: '250px',
+		opacity: '1.0'
+	}, "slow");
+
+	var map, infoWindow;
+
+	function initMap() {
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: {
+				lat: -34.397,
+				lng: 150.644
+			},
+			zoom: 6
+		});
+		infoWindow = new google.maps.InfoWindow;
+
+		// Try HTML5 geolocation.
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function (position) {
+				var pos = {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude
+				};
+
+				infoWindow.setPosition(pos);
+				infoWindow.setContent('Location found.');
+				infoWindow.open(map);
+				map.setCenter(pos);
+			}, function () {
+				handleLocationError(true, infoWindow, map.getCenter());
+			});
+		} else {
+			// Browser doesn't support Geolocation
+			handleLocationError(false, infoWindow, map.getCenter());
+		}
+	}
+
+	function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+		infoWindow.setPosition(pos);
+		infoWindow.setContent(browserHasGeolocation ?
+			'Error: The Geolocation service failed.' :
+			'Error: Your browser doesn\'t support geolocation.');
+		infoWindow.open(map);
+	}
+
+});
