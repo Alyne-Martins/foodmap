14bea15dccd975cbdc03c465683cb2f71cd824cc
Alyne Martins <martins.alyne@gmail.com>
2018-08-06 03:38:18 -0300

    alterações + modal

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
 	$('.home').delay('6000').fadeIn('slow');
 	var boxLogo = $("#box-logo");
 	boxLogo.animate({
-		height: '250px'
+		height: '300px'
 	}, "slow");
 	boxLogo.animate({
-		width: '250px',
+		width: '300px',
 		opacity: '0.4'
 	}, "slow");
 	boxLogo.animate({
@@ -14,44 +14,44 @@ $(document).ready(function () {
 		opacity: '0.6'
 	}, "slow");
 	boxLogo.animate({
-		width: '250px',
+		width: '300px',
 		opacity: '1.0'
 	}, "slow");
 	boxLogo.animate({
-		width: '250px',
-		height: '250px',
+		width: '300px',
+		height: '300px',
 		opacity: '1.0'
 	}, "slow");
 	boxLogo.animate({
-		width: '250px',
-		height: '250px',
 		opacity: '0.6'
 	}, "slow");
 	boxLogo.animate({
-		width: '250px',
-		height: '250px',
 		opacity: '1.0'
 	}, "slow");
 
 	for (var i of restaurantes) {
 		var imgRestaurante = i.image;
-		var imgHtml = '<img class="img-Restaurante mb-2" src="' + imgRestaurante + '" alt="">';
+		var nameId = i.name;
+		var imgHtml = '<div class="d-flex flex-column justify-content-center div-Box mb-4 mx-2" id="' + nameId + '" data-toggle="modal" data-target="#exampleModal"><p class="titulo align-self-center">' + nameId + '</p><img class="align-self-center img-Restaurante" src="' + imgRestaurante + '" alt="" ></div>';
 		$("#box-img").append(imgHtml);
 	};
+
+
 
 	$('.filtrar').click(function () {
 		var typedWord = $('#filter').val();
 		$('.img-Restaurante').remove();
+		$('.titulo').remove();
+		$('.div-Box').remove();
 		for (var restaurante of restaurantes) {
-			if (typedWord == restaurante.type || typedWord == restaurante.name) {
+			if (typedWord === restaurante.type || typedWord === restaurante.name) {
+				var idName = restaurante.name;
 				var imgTyped = restaurante.image;
-				var imgRest = '<img class="img-Restaurante mb-2" src="' + imgTyped + '" alt="">';
+				var imgRest = '<div class="d-flex flex-column justify-content-center div-Box mb-4 mx-2" id="' + idName + '" data-toggle="modal" data-target="#exampleModal"><p class="titulo align-self-center">' + idName + '</p><img class="align-self-center img-Restaurante mb-2" src="' + imgTyped + '" alt="" id="' + idName + '"></div>';
 				$("#box-img").append(imgRest);
 			}
 		}
 	});
-
-
 });
 
 function initMap() {
